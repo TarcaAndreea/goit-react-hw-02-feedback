7dc0e526e4958d961191b2310f399dd4642b060d
TarcaAndreea <adrianatarca1@gmail.com>
2023-08-20 01:16:40 +0300

    step1-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
+import React, { useState } from 'react';
+import { Section } from './Section/section/section';
+import { FeedbackOptions } from './FeedbackOptions/feedback-options';
+import { Statistics } from './Statistics/statistics';
 export const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = type => {
+    setFeedback({
+      ...feedback,
+      [type]: feedback[type] + 1,
+    });
+  };
+  const countTotalFeedback = () => {
+    return feedback.good + feedback.neutral + feedback.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total ? Math.round((feedback.good / total) * 100) : 0;
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title="Te rugăm lasă-ne feedback-ul tău!">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+      <Section title="Statistici">
+        <Statistics
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      </Section>
     </div>
   );
 };
